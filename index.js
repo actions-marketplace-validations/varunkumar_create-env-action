438de60ec36290751d0ac5175cc620baaa53edbc
Varunkumar Nagarajan <varunkumar.n@gmail.com>
2022-12-07 00:31:25 +0530

    Integrate with dotenv

--- index.js
+++ index.js
@@ -1,15 +1,65 @@
 import { getInput, setFailed, setOutput } from '@actions/core';
-import { context } from '@actions/github';
+import * as dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  setOutput('time', time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  const raw = getInput('env');
+  const filename = getInput('file');
+  const pathname = getInput('path');
+
+  // parse the raw input using dotenv
+  const parsed = dotenv.parse(raw);
+  setOutput('parsed', parsed);
+  console.log('Env data parsed');
+
+  const fullpath = path.resolve(__dirname, pathname, filename);
+  setOutput('location', fullpath);
+
+  const generated = format(parsed);
+  fs.writeFileSync(fullpath, generated);
+  console.log('Secrets file created');
+
+  const found = read(fullpath);
+  const sanitized = sanitize(found);
+  setOutput('sanitized', sanitized);
+
+  setOutput('success', true);
 } catch (error) {
   setFailed(error.message);
+}
+
+const VALID = '***';
+const INVALID = 'INVALID';
+
+function sanitize(secrets) {
+  return Object.keys(secrets).reduce((final, key) => {
+    const valid = typeof secrets[key] === 'string' && secrets[key].length > 0;
+
+    return {
+      ...final,
+      [key]: valid ? VALID : INVALID,
+    };
+  }, {});
+}
+
+function format(secrets) {
+  return Object.keys(secrets)
+    .map((key) => {
+      return `${key}=${secrets[key]}`;
+    })
+    .join('\n');
+}
+
+function read(fileLocation) {
+  try {
+    const data = dotenv.config({ path: fileLocation }).parsed;
+    return data;
+  } catch (error) {
+    throw new Error(
+      `Unable to parse dotenv file: ${fileLocation}, ${
+        error ? error.message : ''
+      }`
+    );
+  }
 }
